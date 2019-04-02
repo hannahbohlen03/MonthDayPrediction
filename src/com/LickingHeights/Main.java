a329b5f7869d799932e731153cba8eeac29ce8ef
Hannah <hannah.bohlen.2021@student.lhschools.org>
2019-04-02 07:57:32 -0400

    mModifier
    Added the mModifier method to convert the month if it equals 1 or 2 (January or February).

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,43 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Insert Scanner
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        //Basic Variables
+
+        int h; //Day of Week
+        int q; //Day of Month
+        int m; //Month
+        int year;
+
+        while(true){
+
+            System.out.println("What month were you born? (number value)");
+            m = keyboard.nextInt();
+            mModifier(m);
+            System.out.println();
+
+        }
+    }
+
+
+
+
+    public static int mModifier(int m){
+        if(m == 1){
+            return m = 13;
+        }
+        else if(m == 2){
+            return m = 14;
+        }
+        else return m;
     }
 }
+
